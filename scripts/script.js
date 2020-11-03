102d1dbf991a8dd3a7f9cf6b97ee5fcbe59e3e77
Luuk Eastman <64014142+luukEastman@users.noreply.github.com>
2020-11-03 17:52:02 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+console.log("hallo");
+var deButton = document.querySelector("button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  document.body.classList.toggle("toonMenu");
+  if(body.class=="toonmenu"){
+    deButton.textContent = "X";
+  }
+}
